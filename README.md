9216b2e5948c07d831dc4ab1823cf400c9c9bff6
Mark Watson <me@markwatson.us>
2019-06-17 00:32:14 -0600

    Initial commit w/ proof of concept design.

---
+++ README.md
@@ -0,0 +1,44 @@
+# s3grep
+
+`s3grep` is an overly simple command to scan files in S3 for lines matching a
+ search query. It hacks S3 Select to find lines in a file that contain a search string. 
+
+> NOTE: This is just a proof of concept / toy project for now.
+
+## Motivations
+
+This was written in order to simplify finding lines in a file that match a string. Without this command
+I've resorted to downloading the whole file and piping to grep. This uses a lot of bandwidth, and can be
+slow. And alternative is to use S3 Select to find lines containing a string, and that's exactly what this
+command does. 
+
+Note that there is a cost associated with scanning a file in S3 per byte scanned. If the files are compressed,
+the cost can be lowered as there will be less bytes scanned. 
+
+## Limitations
+
+- We only scan a single file at a time (this could change - ideally it should detect if the path is a single file or prefix, and in the case of a prefix just scan all matching files).
+- Only simple string matches are supported.
+- Files with null bytes aren't supported. 
+- The credentials setup in the default environment are the only ones used. For fancy profiles use something like aws-runas.
+
+## Installation
+
+- Install golang: <https://golang.org>
+- Setup a go environment: <https://golang.org/doc/code.html>
+- Use go get to install
+```bash
+$ go get github.com/markwatson/s3grep
+```
+
+## Usage
+See `s3grep -help`:
+```
+Usage of C:\Users\me\go\bin\s3grep.exe:
+  -match string
+        The text to match on.
+  -path string
+        The S3 path to scan.
+```
+
+
---
+++ s3grep.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"net/url"
+	"os"
+	"strings"
+)
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
+}
+
+func getS3() *s3.S3 {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc := s3.New(sess)
+
+	return svc
+}
+
+func parseS3Path(path string) (error, string, string) {
+	u, err := url.Parse(path)
+
+	if err != nil {
+		return err, "", ""
+	} else if u.Scheme != "s3" {
+		return fmt.Errorf("scheme '%s' is not s3", u.Scheme), "", ""
+	} else {
+		return nil, u.Host, u.Path
+	}
+}
+
+func matchS3(svc *s3.S3, bucket string, key string, exp string) error {
+	compression := "NONE"
+	if strings.HasSuffix(key, ".gz") {
+		compression = "GZIP"
+	} else if strings.HasSuffix(key, ".bz2") {
+		compression = "BZIP2"
+	}
+
+	params := &s3.SelectObjectContentInput{
+		Bucket: aws.String(bucket),
+		Key: aws.String(key),
+		ExpressionType: aws.String(s3.ExpressionTypeSql),
+		Expression: aws.String("select * from s3object s where s._1 like '%" + exp + "%'"),
+		InputSerialization: &s3.InputSerialization{
+			CSV: &s3.CSVInput{
+				FieldDelimiter: aws.String("\000"),
+				RecordDelimiter: aws.String("\n"),
+				FileHeaderInfo: aws.String(s3.FileHeaderInfoNone),
+			},
+			CompressionType: aws.String(compression),
+		},
+		OutputSerialization: &s3.OutputSerialization{
+			CSV: &s3.CSVOutput{
+				QuoteCharacter: aws.String(""),
+				QuoteEscapeCharacter: aws.String(""),
+				FieldDelimiter: aws.String(""),
+			},
+		},
+	}
+
+	resp, err := svc.SelectObjectContent(params)
+	if err != nil {
+		return err
+	}
+	defer resp.EventStream.Close()
+
+	for event := range resp.EventStream.Events() {
+		switch v := event.(type) {
+		case *s3.RecordsEvent:
+			// s3.RecordsEvent.Records is a byte slice of select records
+			fmt.Println(string(v.Payload))
+		}
+	}
+
+	if err := resp.EventStream.Err(); err != nil {
+		return fmt.Errorf("failed to read from SelectObjectContent EventStream, %v", err)
+	}
+
+	return nil
+}
+
+func main() {
+	svc := getS3()
+
+	path := flag.String("path", "", "The S3 path to scan.")
+	match := flag.String("match", "", "The text to match on.")
+
+	flag.Parse()
+	if *path == "" {
+		exitErrorf("You must specify a path. See -help")
+	}
+	if *match == "" {
+		exitErrorf("You must specify text to match on. See -help")
+	}
+
+	err, bucket, key := parseS3Path(*path)
+	if err != nil {
+		exitErrorf("Unable to parse path: %v", err)
+	}
+
+	err = matchS3(svc, bucket, key, *match)
+	if err != nil {
+		exitErrorf("Error scanning: %v", err)
+	}
+}
